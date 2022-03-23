0923115c50ea6bdb92b7c8e0f631ae5b4ec333c8
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-23 22:45:47 +0000

    using "util" header in main

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "Window.h"
+#include "util.h"
 
 int main(int argc, char *argv[])
 {
-    Window_Init(1280, 720);
+    Window_Init(800, 800);
     Window_Loop();
 
     return 0;
