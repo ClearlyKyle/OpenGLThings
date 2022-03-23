2b9d02836ef5b45f97a3ff8b086a35c6bbe93adb
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-23 21:16:47 +0000

    Moved some files

---
+++ BasicTraingle.c
@@ -0,0 +1,5 @@
+
+void Basic_Triangle_Render()
+{
+    
+}
---
+++ Engine/util.h
@@ -0,0 +1,11 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#endif // __UTIL_H__
