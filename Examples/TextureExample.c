6954e87c3ddb710b428c807fcdaa003a2db24ff3
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-25 22:09:57 +0000

    Rendering a texture example

---
+++ Examples/TextureExample.c
@@ -0,0 +1,82 @@
+
+#include "TextureExample.h"
+
+static struct TextureExample texture_example;
+
+void TextureExample_Init()
+{
+    // Vertices coordinates
+    GLfloat vertices[] =
+        {
+            //     COORDINATES  /     COLORS      / TexCoord  //
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Lower left corner
+            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  // Upper left corner
+            0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // Upper right corner
+            0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f   // Lower right corner
+        };
+
+    // Indices for vertices order
+    GLuint indices[] =
+        {
+            0, 2, 1, // Upper triangle
+            0, 3, 2  // Lower triangle
+        };
+
+    // Generates Shader object using shaders default.vert and default.frag
+    struct Shader shader = Shader_Create(
+        "../../shaders/texture_example.vs",
+        "../../shaders/texture_example.fs",
+        2, (struct VertexAttribute[]){{.index = 0, .name = "aPos"}, {.index = 1, .name = "aColor"}});
+
+    texture_example.shader = shader;
+
+    // Generates Vertex Array Object and binds it
+    struct VAO vao = VAO_Create();
+    VAO_Bind(vao);
+    texture_example.vao = vao;
+
+    // Generates Vertex Buffer Object and links it to vertices
+    struct VBO vbo = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(vbo, sizeof(vertices), (const GLvoid *)vertices);
+
+    // Generates Element Buffer Object and links it to indices
+    struct EBO ebo = EBO_Create();
+    EBO_Buffer(ebo, sizeof(indices), (const GLvoid *)indices);
+
+    // Links VBO attributes such as coordinates and colors to VAO
+    VAO_Attr(vao, vbo, 0, 3, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)0);
+    VAO_Attr(vao, vbo, 1, 3, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)(3 * sizeof(GLfloat)));
+    VAO_Attr(vao, vbo, 2, 2, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)(6 * sizeof(GLfloat)));
+
+    // Texture
+    const char *file_path = "../../Examples/Textures/stone.png";
+    struct Texture tex = Texture_Create(file_path, GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    texture_example.tex = tex;
+
+    Texture_Uniform(shader, "tex0", 0);
+
+    // Unbind all to prevent accidentally modifying them
+    // VAO_Unbind();
+    // VBO_Unbind();
+    // EBO_Unbind();
+}
+void TextureExample_Update()
+{
+    // Gets ID of uniform called "scale"
+
+    Shader_Bind(texture_example.shader);
+
+    // Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+    Shader_Uniform_Float(texture_example.shader, "scale", 0.5f);
+    // OR
+    // glUniform1f(glGetUniformLocation(texture_example.shader.shader_id, "scale"), 0.5f);
+
+    // Binds texture so that is appears in rendering
+    Texture_Bind(texture_example.tex);
+
+    // Bind the VAO so OpenGL knows to use it
+    VAO_Bind(texture_example.vao);
+
+    // Draw primitives, number of indices, datatype of indices, index of indices
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
---
+++ Examples/TextureExample.h
@@ -0,0 +1,16 @@
+#ifndef __TEXTUREEXAMPLE_H__
+#define __TEXTUREEXAMPLE_H__
+
+#include "util.h"
+
+struct TextureExample
+{
+    struct Shader shader;
+    struct VAO vao;
+    struct Texture tex;
+};
+
+void TextureExample_Init();
+void TextureExample_Update();
+
+#endif // __TEXTUREEXAMPLE_H__
