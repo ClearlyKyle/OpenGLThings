88d85acb5857a1c3e202c0a021439e7fe9663efb
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-25 22:11:10 +0000

    change to "util.h"

--- Engine/Window.h
+++ Engine/Window.h
@@ -1,15 +1,9 @@
 #ifndef __WINDOW_H__
 #define __WINDOW_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
+#include "util.h"
 
 #include "SDL2/SDL.h"
-#include "glad/glad.h"
-
-#include "..\BasicTriangle.h"
 
 typedef void (*Windowfunction_ptr)();
 
