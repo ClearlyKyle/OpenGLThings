f167a6d67e032956b57aa1a182d19f496784173a
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-26 22:14:11 +0000

    reformat headers

--- Engine/util.h
+++ Engine/util.h
@@ -7,13 +7,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <stdint.h>
 
 #include "glad/glad.h"
-//#include "cglm/cglm.h"
-#include <cglm/cglm.h>
-#include <cglm/struct.h>
-#include <cglm/call.h>
+#include "cglm/cglm.h"
+#include "cglm/struct.h"
+#include "cglm/call.h"
 
+#include "SDL2/SDL.h"
+
+#include "Inputs.h"
+#include "Camera.h"
 #include "Shaders.h"
 #include "VBO.h"
 #include "EBO.h"
