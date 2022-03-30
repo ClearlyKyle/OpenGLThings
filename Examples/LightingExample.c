29f62463c2108ddc3f7efc5e9a388b23608f5382
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-03-30 21:33:33 +0100

    Fixed bugs, issue with passing a vec3 to uniform4f

--- Examples/LightingExample.c
+++ Examples/LightingExample.c
@@ -30,7 +30,6 @@ void LightingExample_Init()
             -0.5f, 0.0f, 0.5f, 0.83f, 0.70f, 0.44f, 0.0f, 0.0f, 0.0f, 0.5f, 0.8f, // Facing side
             0.0f, 0.8f, 0.0f, 0.92f, 0.86f, 0.76f, 2.5f, 5.0f, 0.0f, 0.5f, 0.8f   // Facing side
         };
-
     // Indices for vertices order
     GLuint indices[] =
         {
@@ -68,94 +67,94 @@ void LightingExample_Init()
             4, 5, 6,
             4, 6, 7};
 
-    {
-        // Generates Shader object using shaders default.vert and default.frag
-        struct Shader shader1 = Shader_Create(
-            "../../shaders/default.vs",
-            "../../shaders/default.fs",
-            3,
-            (struct VertexAttribute[]){
-                {.index = 0, .name = "aPos"},
-                {.index = 1, .name = "aColor"},
-                {.index = 2, .name = "aTex"},
-                {.index = 3, .name = "aNormal"}});
+    /* PYRAMID SAHDER ---------------------------------------------------------------------*/
+    // Generates Shader object using shaders default.vert and default.frag
+    struct Shader shader1 = Shader_Create(
+        "../../shaders/default.vs",
+        "../../shaders/default.fs",
+        4,
+        (struct VertexAttribute[]){
+            {.index = 0, .name = "aPos"},
+            {.index = 1, .name = "aColor"},
+            {.index = 2, .name = "aTex"},
+            {.index = 3, .name = "aNormal"}});
 
-        lights.shader[0] = shader1;
+    lights.shader[0] = shader1;
 
-        // Generates Vertex Array Object and binds it
-        struct VAO vao1 = VAO_Create();
-        VAO_Bind(vao1);
-        lights.vao[0] = vao1;
+    // Generates Vertex Array Object and binds it
+    struct VAO vao1 = VAO_Create();
+    VAO_Bind(vao1);
 
-        // Generates Vertex Buffer Object and links it to vertices
-        struct VBO vbo = VBO_Create(GL_ARRAY_BUFFER);
-        VBO_Buffer(vbo, sizeof(vertices), (const GLvoid *)vertices);
+    // Generates Vertex Buffer Object and links it to vertices
+    struct VBO vbo = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(vbo, sizeof(vertices), (const GLvoid *)vertices);
 
-        struct EBO ebo = EBO_Create();
-        EBO_Buffer(ebo, sizeof(indices), (void *)indices);
+    struct EBO ebo = EBO_Create();
+    EBO_Buffer(ebo, sizeof(indices), (void *)indices);
 
-        // Links VBO attributes such as coordinates and colors to VAO
-        VAO_Attr(vao1, vbo, 0, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(0));
-        VAO_Attr(vao1, vbo, 1, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(3 * sizeof(GLfloat)));
-        VAO_Attr(vao1, vbo, 2, 2, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(6 * sizeof(GLfloat)));
-        VAO_Attr(vao1, vbo, 3, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(8 * sizeof(GLfloat)));
+    // Links VBO attributes such as coordinates and colors to VAO
+    VAO_Attr(vao1, vbo, 0, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(0));
+    VAO_Attr(vao1, vbo, 1, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(3 * sizeof(GLfloat)));
+    VAO_Attr(vao1, vbo, 2, 2, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(6 * sizeof(GLfloat)));
+    VAO_Attr(vao1, vbo, 3, 3, GL_FLOAT, 11 * sizeof(GLfloat), (const GLvoid *)(8 * sizeof(GLfloat)));
 
-        // Unbind all to prevent accidentally modifying them
-        VAO_Unbind();
-        VBO_Unbind();
-        EBO_Unbind();
-    }
+    lights.vao[0] = vao1;
+    // Unbind all to prevent accidentally modifying them
+    VBO_Unbind();
+    VAO_Unbind();
+    // EBO_Unbind();
+    /* PYRAMID SAHDER ---------------------------------------------------------------------*/
 
-    { // Shader for light cube
-        struct Shader shader2 = Shader_Create(
-            "../../shaders/lights.vs",
-            "../../shaders/lights.fs",
-            3,
-            (struct VertexAttribute[]){
-                {.index = 0, .name = "aPos"},
-                {.index = 1, .name = "aColor"},
-                {.index = 2, .name = "aTex"}});
+    /* LIGHT SAHDER ---------------------------------------------------------------------*/
+    // Shader for light cube
+    struct Shader shader2 = Shader_Create(
+        "../../shaders/lights.vs",
+        "../../shaders/lights.fs",
+        1,
+        (struct VertexAttribute[]){
+            {.index = 0, .name = "aPos"}});
 
-        lights.shader[1] = shader2;
+    lights.shader[1] = shader2;
 
-        // Generates Vertex Array Object and binds it
-        struct VAO vao2 = VAO_Create();
-        VAO_Bind(vao2);
-        lights.vao[1] = vao2;
+    // Generates Vertex Array Object and binds it
+    struct VAO vao2 = VAO_Create();
+    VAO_Bind(vao2);
 
-        // Generates Vertex Buffer Object and links it to vertices
-        struct VBO vbo2 = VBO_Create(GL_ARRAY_BUFFER);
-        VBO_Buffer(vbo2, sizeof(lightVertices), (const GLvoid *)lightVertices);
+    // Generates Vertex Buffer Object and links it to vertices
+    struct VBO vbo2 = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(vbo2, sizeof(lightVertices), (const GLvoid *)lightVertices);
 
-        struct EBO ebo2 = EBO_Create();
-        EBO_Buffer(ebo2, sizeof(lightIndices), (void *)lightIndices);
+    struct EBO ebo2 = EBO_Create();
+    EBO_Buffer(ebo2, sizeof(lightIndices), (void *)lightIndices);
 
-        // Links VBO attributes such as coordinates and colors to VAO
-        VAO_Attr(vao2, vbo2, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)(0));
+    // Links VBO attributes such as coordinates and colors to VAO
+    VAO_Attr(vao2, vbo2, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)(0));
+    lights.vao[1] = vao2;
 
-        // Unbind all to prevent accidentally modifying them
-        VAO_Unbind();
-        VBO_Unbind();
-        EBO_Unbind();
-    }
+    // Unbind all to prevent accidentally modifying them
+    VBO_Unbind();
+    VAO_Unbind();
+    // EBO_Unbind();
+    /* LIGHT SAHDER ---------------------------------------------------------------------*/
 
     vec4 light_colour = {1.0f, 1.0f, 1.0f, 1.0f};
-    vec3 light_position = {0.5f, 0.5f, 1.5f};
-    mat4 light_model = GLM_MAT4_ZERO_INIT;
+    vec3 light_position = {0.5f, 0.5f, 0.5f};
+    mat4 light_model;
     glm_translate_make(light_model, light_position);
 
     vec3 pyramid_position = {0.0f, 0.0f, 0.0f};
-    mat4 pyramid_model = GLM_MAT4_ZERO_INIT;
+    mat4 pyramid_model;
     glm_translate_make(pyramid_model, pyramid_position);
 
-    Shader_Bind(lights.shader[0]);
-    Shader_Uniform_Mat4(lights.shader[0], "model", light_model);
-    Shader_Uniform_Vec4(lights.shader[0], "lightColor", light_colour);
-
     Shader_Bind(lights.shader[1]);
-    Shader_Uniform_Mat4(lights.shader[1], "model", pyramid_model);
+    Shader_Uniform_Mat4(lights.shader[1], "model", light_model);
     Shader_Uniform_Vec4(lights.shader[1], "lightColor", light_colour);
-    Shader_Uniform_Vec4(lights.shader[1], "lightPos", light_position);
+
+    Shader_Bind(lights.shader[0]);
+    Shader_Uniform_Mat4(lights.shader[0], "model", pyramid_model);
+    Shader_Uniform_Vec4(lights.shader[0], "lightColor", light_colour);
+    // ALWATS CHECK WHAT VEC SIZE SENDING TO UNIFORM, VEC3? VEC4?
+    Shader_Uniform_Vec3(lights.shader[0], "lightPos", light_position);
 
     // Texture
     const char *file_path = "../../Examples/Textures/brick.png";
@@ -188,6 +187,7 @@ void LightingExample_Update()
 
     // Bind the VAO so OpenGL knows to use it
     VAO_Bind(lights.vao[0]);
+
     // Draw primitives, number of indices, datatype of indices, index of indices
     glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
 
