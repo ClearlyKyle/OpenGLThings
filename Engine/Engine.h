b22ee291d3fa8cc16fcfc428fdb29017262084b6
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-04-03 22:46:11 +0100

    Engine header file

---
+++ Engine/Engine.h
@@ -0,0 +1,11 @@
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+#include "VAO.h"
+#include "VBO.h"
+#include "Shaders.h"
+#include "Textures.h"
+#include "ModelLoader.h"
+#include "Camera.h"
+
+#endif // __ENGINE_H__
