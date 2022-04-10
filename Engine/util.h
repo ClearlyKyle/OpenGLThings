0d5b2a3c373a2c8d97c00c504c43c5bbd4d6d742
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-04-10 22:06:09 +0100

    alphabetical order

--- Engine/util.h
+++ Engine/util.h
@@ -2,13 +2,13 @@
 #define __UTIL2_H__
 
 // C STD
+#include <assert.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <stdint.h>
 
 // OPENGL
 #include "glad/glad.h"
