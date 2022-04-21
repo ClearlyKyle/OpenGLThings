ddea5755b10b492a111c6450fd58f22514d03ae6
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-04-21 22:37:14 +0100

    instancing example

---
+++ Examples/16 - Instancing/Instancing.h
@@ -0,0 +1,10 @@
+#ifndef __INSTANCING_H__
+#define __INSTANCING_H__
+
+#include "Engine.h"
+
+void Instancing_Init();
+void Instancing_Update();
+void Instancing_OnExit();
+
+#endif // __INSTANCING_H__
