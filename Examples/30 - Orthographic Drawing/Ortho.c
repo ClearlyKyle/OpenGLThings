14bcb8fe570cf5de18f4ac7ef8997a4f164b0548
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-04-26 22:26:49 +0100

    orthographic projection example

---
+++ Examples/30 - Orthographic Drawing/Ortho.c
@@ -0,0 +1,106 @@
+#include "Ortho.h"
+
+static struct OrthoDrawing
+{
+    GLuint VAO;
+
+    struct Shader shader;
+} od;
+
+void Ortho_Init()
+{
+    const GLfloat cube_vertices[] = {
+        // back face
+        -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,   // top-right
+        1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f,  // bottom-right
+        1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,   // top-right
+        -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+        -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,  // top-left
+        // front face
+        -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left
+        1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,  // bottom-right
+        1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // top-right
+        1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // top-right
+        -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // top-left
+        -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left
+        // left face
+        -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   // top-right
+        -1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top-left
+        -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-left
+        -1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,  // bottom-right
+        -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   // top-right
+                                                            // right face
+        1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,     // top-left
+        1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,   // bottom-right
+        1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,    // top-right
+        1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,   // bottom-right
+        1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,     // top-left
+        1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,    // bottom-left
+        // bottom face
+        -1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+        1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f,  // top-left
+        1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,   // bottom-left
+        1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,   // bottom-left
+        -1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f,  // bottom-right
+        -1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+        // top face
+        -1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top-left
+        1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // bottom-right
+        1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,  // top-right
+        1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // bottom-right
+        -1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top-left
+        -1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f   // bottom-left
+    };
+
+    struct Shader shader_ortho = Shader_Create("../../Examples/30 - Orthographic Drawing/orthoshader.vs",
+                                               "../../Examples/30 - Orthographic Drawing/orthoshader.fs",
+                                               1,
+                                               (struct VertexAttribute[]){
+                                                   {.index = 0, .name = "position"}});
+
+    od.shader = shader_ortho;
+
+    // CUBE
+    struct VAO cube_VAO = VAO_Create();
+    struct VBO cube_VBO = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(cube_VBO, sizeof(cube_vertices), (const GLvoid *)cube_vertices);
+    VAO_Attr(cube_VAO, cube_VBO, 0, 3, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)(0));
+    VAO_Attr(cube_VAO, cube_VBO, 1, 3, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)(3 * sizeof(GLfloat)));
+    VAO_Attr(cube_VAO, cube_VBO, 2, 2, GL_FLOAT, 8 * sizeof(GLfloat), (const GLvoid *)(6 * sizeof(GLfloat)));
+
+    od.VAO = cube_VAO.ID;
+
+    VAO_Unbind();
+    VBO_Unbind();
+
+    // Model setup
+    const float scale  = 1.0f;
+    const float aspect = window.aspect_ratio;
+    const float zNear  = 0.1f;
+    const float zFar   = 100.0f;
+    mat4        ortho_mat;
+    mat4        view;
+    mat4        position;
+
+    glm_ortho(-aspect * scale, aspect * scale, -scale, scale, zNear, zFar, ortho_mat);
+
+    glm_ortho(0.0f, window.width, 0.0f, window.heigh, 0.1f, 100.0f, light_orthogonal_projection);
+    // glm_ortho(-35.0f, 35.0f, -35.0f, 35.0f, 0.1f, 75.0f, light_orthogonal_projection);
+    glm_lookat(light_position, (vec3){0.0f, 0.0f, 0.0f}, (vec3){0.0f, 1.0f, 0.0f}, light_view);
+    glm_mat4_mul(light_view, light_orthogonal_projection, light_projection);
+}
+
+void Ortho_Update()
+{
+
+    // Draw Cube
+    glBindVertexArray(od.VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0); // Unbind
+}
+
+void Ortho_OnExit()
+{
+}
---
+++ Examples/30 - Orthographic Drawing/Ortho.h
@@ -0,0 +1,10 @@
+#ifndef __ORTHO_H__
+#define __ORTHO_H__
+
+#include "Engine.h"
+
+void Ortho_Init();
+void Ortho_Update();
+void Ortho_OnExit();
+
+#endif // __ORTHO_H__
