17c5fd71c33cca90ef0e8c757d6ff9fac67c6c8c
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-05-07 19:03:43 +0100

    Shadow map with pointlights

---
+++ Examples/22 - Shadow Maps (Spotlights & Point Lights)/SM_With_SP_PL.c
@@ -0,0 +1,16 @@
+#include "SM_With_SP_PL.h"
+
+void ShadowMapLights_Init()
+{
+    
+}
+
+void ShadowMapLights_Update()
+{
+    
+}
+
+void ShadowMapLights_OnExit()
+{
+    
+}
---
+++ Examples/22 - Shadow Maps (Spotlights & Point Lights)/SM_With_SP_PL.h
@@ -0,0 +1,10 @@
+#ifndef __SM_WITH_SP_PL_H__
+#define __SM_WITH_SP_PL_H__
+
+#include "Engine.h"
+
+void ShadowMapLights_Init();
+void ShadowMapLights_Update();
+void ShadowMapLights_OnExit();
+
+#endif // __SM_WITH_SP_PL_H__
