c2486d0b223e0cc7b425a127e67c2cf5b89556ce
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-05-10 22:32:05 +0100

    udpate file path

--- Examples/9 - Depth Buffer/DepthBuffer.c
+++ Examples/9 - Depth Buffer/DepthBuffer.c
@@ -5,8 +5,8 @@ static struct DepthBuffer dbuffer;
 void DepthBuffer_Init()
 {
     struct Shader shader = Shader_Create(
-        "../../Examples/shaders/9/depthbuffer.vs",
-        "../../Examples/shaders/9/depthbuffer.fs",
+        "../../Examples/9 - Depth Buffer/depthbuffer.vs",
+        "../../Examples/9 - Depth Buffer/depthbuffer.fs",
         3,
         (struct VertexAttribute[]){
             {.index = 0, .name = "aPos"},
@@ -14,7 +14,7 @@ void DepthBuffer_Init()
             {.index = 2, .name = "aTex"},
             {.index = 3, .name = "aColour"}});
 
-    vec4 light_colour = {1.0f, 1.0f, 1.0f, 1.0f};
+    vec4 light_colour   = {1.0f, 1.0f, 1.0f, 1.0f};
     vec3 light_position = {0.5f, 0.5f, 0.5f};
     mat4 light_model;
     glm_translate_make(light_model, light_position);
@@ -26,7 +26,7 @@ void DepthBuffer_Init()
     // struct Mesh mesh = Mesh_load(shader, "../../Examples/res/models/Wooden Box/wooden crate.obj");
     // struct Mesh mesh = Mesh_load(shader, "../../Examples/res/models/Dog House/DogHouse.obj");
     struct Mesh mesh = Mesh_Load(shader, "../../Examples/res/models/Low Poly Scene/Low-Poly_Models.obj");
-    dbuffer.model = mesh;
+    dbuffer.model    = mesh;
 
     // Camera
     struct Camera cam = Camera_Create(window.width, window.heigh, (vec3){0.0f, 0.0f, 2.0f}, 45.0f, 0.1f, 100.0f);
@@ -35,7 +35,7 @@ void DepthBuffer_Init()
     glm_vec3_copy((vec3){0.683066f, -0.107999f, -0.722327f}, cam.orientation);
     // glm_vec3_copy((vec3){0.000000f, 1.000000f, 0.000000f}, cam.up);
     cam.pitch = -6.199975f;
-    cam.yaw = -46.600174f;
+    cam.yaw   = -46.600174f;
 
     dbuffer.cam = cam;
 }
