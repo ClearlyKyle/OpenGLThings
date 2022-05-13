5b18845ea9cc83abcb07f10c72dda31ea4a78d44
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-05-13 21:37:53 +0100

    updated face culling example

--- Examples/11 - Face Culling/FaceCulling.c
+++ Examples/11 - Face Culling/FaceCulling.c
@@ -12,8 +12,9 @@ void FaceCulling_Init()
         (struct VertexAttribute[]){
             {.index = 0, .name = "aPos"},
             {.index = 1, .name = "aNormal"},
-            {.index = 2, .name = "aColour"},
-            {.index = 3, .name = "aTex"}});
+            {.index = 2, .name = "aTex"},
+            {.index = 3, .name = "aColor"},
+        });
 
     // Take care of all the light related things
     vec4 lightColor = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -21,21 +22,18 @@ void FaceCulling_Init()
     // mat4 lightModel;
     // glm_translate_make(lightModel, lightPos);
 
-    Shader_Bind(shaderProgram);
-    Shader_Uniform_Vec4(shaderProgram, "lightColor", lightColor);
-    Shader_Uniform_Vec3(shaderProgram, "lightPos", lightPos);
-
     // Enables the Depth Buffer
-    // glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
 
     // CULLING
     glEnable(GL_CULL_FACE); // Enables Cull Facing
                             // glCullFace(GL_FRONT);
-    glCullFace(GL_BACK);    // specifies which face to cull
+    glCullFace(GL_FRONT);   // specifies which face to cull
     glFrontFace(GL_CCW);    // Uses counter clock-wise standard
 
     // struct Mesh model = Mesh_Load("S:/Downloads/170516_mia337_032122_600_200Kfaces_8192px_zip/170516_mia337_032122_600_200Kfaces_8192px_OBJ.obj");
-    struct Mesh model = Mesh_Load(shaderProgram, "../../Examples/res/models/statue/scene.gltf");
+    // struct Mesh model = Mesh_Load(shaderProgram, "../../Examples/res/models/statue/scene.gltf");
+    struct Mesh model = Mesh_Load(shaderProgram, "../../Examples/res/models/Low Poly Scene/Low-Poly_Models.obj");
     // struct Mesh model = Mesh_Load(shaderProgram, "../../Examples/res/models/Wooden Box/wooden crate.obj");
     fc.model = model;
 
@@ -49,9 +47,14 @@ void FaceCulling_Init()
 
     fc.cam = cam;
 
+    Shader_Bind(shaderProgram);
+    Shader_Uniform_Vec4(shaderProgram, "lightColor", lightColor);
+    Shader_Uniform_Vec3(shaderProgram, "lightPos", lightPos);
+    Shader_Uniform_Vec3(shaderProgram, "camPos", cam.position);
+
     // Texture_Uniform(fc.model.shader, fc.model.textures[0], "diffuse0", fc.model.textures[0].slot);
 
-    Shader_Uniform_Texture2D(fc.model.shader, "diffuse0", fc.model.texInfo.textures[0]);
+    // Shader_Uniform_Texture2D(fc.model.shader, "diffuse0", fc.model.texInfo.textures[0]);
 }
 
 void FaceCulling_Update()
@@ -59,7 +62,8 @@ void FaceCulling_Update()
     // fprintf(stderr, "%fms : %f FPS\n", window.frame_time, 1.0f / window.frame_time);
     Camera_Inputs(&fc.cam);
 
-    Shader_Uniform_Vec3(fc.model.shader, "camPos", fc.cam.position);
+    // Shader_Bind(fc.model.shader);
+    // Shader_Uniform_Vec3(fc.model.shader, "camPos", fc.cam.position);
     Camera_View_Projection_To_Shader(fc.cam, fc.model.shader, "camMatrix");
 
     Mesh_Draw(fc.model);
