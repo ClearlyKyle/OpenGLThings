d8b68645695b1f14b8370f187c380390fdd92b8c
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-06-07 22:46:00 +0100

    line and plane examples

--- Examples/27 - Procedural/Procedural.c
+++ Examples/27 - Procedural/Procedural.c
@@ -8,6 +8,15 @@ static struct Procedural
     Shader_t circle_Shader;
     VAO_t    circle_VAO;
     GLuint   circle_Count;
+
+    Shader_t line_Shader;
+    VAO_t    line_VAO;
+    GLuint   line_Indicies;
+
+    Shader_t plane_Shader;
+    VAO_t    plane_VAO;
+    GLuint   plane_Indicies;
+
 } pc;
 
 static float FLOAT_RAND2(const float min, const float max)
@@ -82,6 +91,99 @@ GLfloat *get_plane_vertices(int width, int height)
     return vertices;
 }
 
+GLuint *get_plane_Indices(GLuint width, GLuint height, GLuint *num_of_indicies_return)
+{
+    //*num_of_indicies_return = (width * height) + (width - 1) * (height - 2);
+    //*num_of_indicies_return = (width + 1) * (height + 1);
+
+    const int numIndPerRow    = width - 1;
+    const int numIndDegensReq = (height - 1) * 2;
+    const int total_indices   = numIndPerRow * height + numIndDegensReq;
+
+    // Now build the index data
+    const int numStripsRequired = width - 1;
+    const int numDegensRequired = 2 * (numStripsRequired - 1);
+    const int verticesPerStrip  = 2 * height;
+
+    *num_of_indicies_return = (verticesPerStrip * numStripsRequired) + numDegensRequired;
+    //*num_of_indicies_return = 2 * (width - 1) * (height - 1);
+    //*num_of_indicies_return = (width * height) + (width - 1) * (height - 2);
+
+    GLuint *indices = (GLuint *)malloc(sizeof(GLuint) * (*num_of_indicies_return));
+
+    unsigned int offset = 0;
+    for (unsigned int y = 0; y < width - 1; y++)
+    {
+        if (y > 0)
+        {
+            // Degenerate begin: repeat first vertex
+            indices[offset++] = (y * width);
+        }
+
+        for (unsigned int x = 0; x < height; x++)
+        {
+            // One part of the strip
+            indices[offset++] = ((y * width) + x);
+            indices[offset++] = (((y + 1) * width) + x);
+        }
+
+        if (y < width - 2)
+        {
+            // Degenerate end: repeat last vertex
+            indices[offset++] = (((y + 1) * width) + (height - 1));
+        }
+    }
+
+    // unsigned int i = 0;
+    // for (unsigned int row = 0; row < height - 1; row++)
+    //{
+    //     if ((row & 1) == 0)
+    //     { // even rows
+    //         for (unsigned int col = 0; col < width; col++)
+    //         {
+    //             indices[i++] = col + row * width; // Puts in the | lines
+    //             indices[i++] = col + (row + 1) * width;
+    //         }
+    //     }
+    //     else
+    //     { // odd rows
+    //         for (unsigned int col = width - 1; col > 0; col--)
+    //         {
+    //             indices[i++] = col + (row + 1) * width;
+    //             indices[i++] = (col - 1) + row * width;
+    //         }
+    //     }
+    // }
+
+    for (size_t i = 0; i < *num_of_indicies_return; i++)
+    {
+        printf("%d\n", indices[i]);
+    }
+
+    // unsigned int index = 0;
+    // for (unsigned int j = 0; j < height - 1; j++)
+    //{
+    //     // Set idx to point at first vertex of row j
+    //     unsigned int idx = j * width;
+
+    //    for (unsigned int i = 0; i < width - 1; i++)
+    //    {
+    //        // Bottom triangle of the quad
+    //        indices[index++] = (idx);
+    //        indices[index++] = (idx + 1);
+    //        indices[index++] = (idx + width);
+    //        // Top triangle of the quad
+    //        indices[index++] = (idx + 1);
+    //        indices[index++] = (idx + width + 1);
+    //        indices[index++] = (idx + width);
+    //        // Move one vertex to the right
+    //        idx++;
+    //    }
+    //}
+
+    return indices;
+}
+
 GLfloat *Plane_Verticies(vec3 v0, vec3 v1, vec3 v2, vec3 v3, const GLuint divisions)
 {
     // v3 --- v2
@@ -94,83 +196,165 @@ GLfloat *Plane_Verticies(vec3 v0, vec3 v1, vec3 v2, vec3 v3, const GLuint divisi
     glm_vec3_sub(v3, v0, vec03);
     glm_vec3_sub(v2, v1, vec12);
 
-    glm_vec3_divs(vec03, divisions, vec03);
-    glm_vec3_divs(vec12, divisions, vec12);
+    glm_vec3_divs(vec03, (float)divisions, vec03);
+    glm_vec3_divs(vec12, (float)divisions, vec12);
 
-    //                                 POSITIONS             TEX
-    GLfloat *plane = (GLfloat *)malloc(sizeof(GLfloat) * 3);
+    //                                            POSITIONS(3) TEX(2)
+    GLfloat *plane = (GLfloat *)malloc((sizeof(GLfloat) * 3) * (divisions + 1) * (divisions + 1));
     // GLfloat *plane = (GLfloat *)malloc(sizeof(GLfloat) * 3 + sizeof(GLfloat) * 2);
 
+    int i = 0;
     for (unsigned int row = 0; row < divisions + 1; row++)
     {
         vec3 start = {
-            v0[0] + vec03[0] * row,
-            v0[1] + vec03[1] * row,
-            v0[2] + vec03[2] * row,
+            v0[0] + (vec03[0] * row),
+            v0[1] + (vec03[1] * row),
+            v0[2] + (vec03[2] * row),
         };
 
         vec3 end = {
-            v1[0] + vec12[0] * row,
-            v1[1] + vec12[1] * row,
-            v1[2] + vec12[2] * row,
+            v1[0] + (vec12[0] * row),
+            v1[1] + (vec12[1] * row),
+            v1[2] + (vec12[2] * row),
         };
 
         vec3 vector_div = {
-            (end[0] + start[0]) / divisions,
-            (end[1] + start[1]) / divisions,
-            (end[2] + start[2]) / divisions,
+            (end[0] - start[0]) / (float)divisions,
+            (end[1] - start[1]) / (float)divisions,
+            (end[2] - start[2]) / (float)divisions,
         };
 
         for (unsigned int col = 0; col < divisions + 1; col++)
         {
             vec3 current_vec = {
-                start[0] + vector_div[0] * col,
-                start[1] + vector_div[1] * col,
-                start[2] + vector_div[2] * col,
+                start[0] + (vector_div[0] * col),
+                start[1] + (vector_div[1] * col),
+                start[2] + (vector_div[2] * col),
             };
 
-            plane[(divisions * col + row) + 0] = current_vec[0];
-            plane[(divisions * col + row) + 1] = current_vec[1];
-            plane[(divisions * col + row) + 2] = current_vec[2];
+            const int index  = 3 * (row * (divisions + 1) + col);
+            plane[index + 0] = current_vec[0];
+            plane[index + 1] = current_vec[1];
+            plane[index + 2] = current_vec[2];
         }
     }
+
+    return plane;
 }
 
 GLuint *Plane_Indicies(const GLuint divisions)
 {
-    
+    return NULL;
+}
+
+GLfloat *Line_Vertices(vec3 start, vec3 end, const GLuint divison)
+{
+    GLfloat *verts_output = (GLfloat *)malloc((sizeof(GLfloat) * 3) * (divison + 1));
+
+    vec3 vec_divison = {
+        (end[0] - start[0]) / (float)divison,
+        (end[1] - start[1]) / (float)divison,
+        (end[2] - start[2]) / (float)divison,
+    };
+
+    for (unsigned int i = 0; i < divison + 1; i++)
+    {
+        vec3 current_vec = {
+            start[0] + (vec_divison[0] * i),
+            start[1] + (vec_divison[1] * i),
+            start[2] + (vec_divison[2] * i),
+        };
+
+        verts_output[3 * i + 0] = current_vec[0];
+        verts_output[3 * i + 1] = current_vec[1];
+        verts_output[3 * i + 2] = current_vec[2];
+    }
+
+    return verts_output;
+}
+
+GLuint *Line_Indices(const GLuint divison)
+{
+    GLuint *indice_output = (GLuint *)malloc(sizeof(GLuint) * (divison + 1));
+
+    for (GLuint i = 0; i < divison + 1; i++)
+    {
+        indice_output[i] = i;
+    }
+
+    return indice_output;
 }
 
 void Procedural_Init()
 {
     srand((unsigned int)time(NULL));
 
-    const Shader_t circle_Shader = Shader_Create("../../Examples/27 - Procedural/circle.vs",
-                                                 "../../Examples/27 - Procedural/circle.fs",
-                                                 1,
-                                                 (struct VertexAttribute[]){
-                                                     {.index = 0, .name = "aPos"},
-                                                 });
+    { // CIRCLE
+        const Shader_t circle_Shader = Shader_Create("../../Examples/27 - Procedural/circle.vs",
+                                                     "../../Examples/27 - Procedural/circle.fs",
+                                                     1,
+                                                     (struct VertexAttribute[]){
+                                                         {.index = 0, .name = "aPos"},
+                                                     });
 
-    const GLuint num_of_triangles = 100;
-    const float  circle_radius    = 0.5f;
+        const GLuint num_of_triangles = 100;
+        const float  circle_radius    = 0.5f;
 
-    GLfloat *verticies = Circle_Verticies(num_of_triangles, circle_radius);
-    GLuint  *indicies  = Circle_Indicies(num_of_triangles);
+        GLfloat *verticies = Circle_Verticies(num_of_triangles, circle_radius);
+        GLuint  *indicies  = Circle_Indicies(num_of_triangles);
 
-    VAO_t circle_VAO = VAO_Create();
-    VBO_t circle_VBO = VBO_Create(GL_ARRAY_BUFFER);
-    EBO_t cricle_EBO = EBO_Create();
+        VAO_t circle_VAO = VAO_Create();
+        VBO_t circle_VBO = VBO_Create(GL_ARRAY_BUFFER);
+        EBO_t cricle_EBO = EBO_Create();
 
-    VBO_Buffer(circle_VBO, sizeof(verticies) * num_of_triangles * 3, (const GLvoid *)verticies);
-    EBO_Buffer(cricle_EBO, sizeof(indicies) * num_of_triangles, (const GLvoid *)indicies);
+        VBO_Buffer(circle_VBO, sizeof(verticies) * num_of_triangles * 3, (const GLvoid *)verticies);
+        EBO_Buffer(cricle_EBO, sizeof(indicies) * num_of_triangles, (const GLvoid *)indicies);
 
-    VAO_Attr(circle_VAO, circle_VBO, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)0);
+        VAO_Attr(circle_VAO, circle_VBO, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)0);
 
-    pc.circle_Shader = circle_Shader;
-    pc.circle_VAO    = circle_VAO;
-    pc.circle_Count  = num_of_triangles;
+        free(verticies);
+        free(indicies);
+
+        pc.circle_Shader = circle_Shader;
+        pc.circle_VAO    = circle_VAO;
+        pc.circle_Count  = num_of_triangles;
     } // CIRCLE
+
+    { // LINE
+        const Shader_t line_shader = Shader_Create("../../Examples/27 - Procedural/line.vs",
+                                                   "../../Examples/27 - Procedural/line.fs",
+                                                   1,
+                                                   (struct VertexAttribute[]){
+                                                       {.index = 0, .name = "aPos"},
+                                                   });
+
+        const GLuint divisons = 10;
+        vec3         start    = {-0.5f, 0.5f, 0.0f};
+        vec3         end      = {0.5f, 0.5f, 0.0f};
+
+        GLfloat *vertices = Line_Vertices(start, end, divisons);
+        GLuint  *indices  = Line_Indices(divisons);
+
+        const GLuint num_of_verts = divisons + 1;
+
+        VAO_t line_VAO = VAO_Create();
+
+        VBO_t line_VBO = VBO_Create(GL_ARRAY_BUFFER);
+        VBO_Buffer(line_VBO, sizeof(GLfloat) * 3 * num_of_verts, (const GLvoid *)vertices);
+
+        EBO_t line_EBO = EBO_Create();
+        EBO_Buffer(line_EBO, sizeof(GLuint) * num_of_verts, (const GLvoid *)indices);
+
+        VAO_Attr(line_VAO, line_VBO, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)0);
+
+        free(vertices);
+        free(indices);
+
+        pc.line_Shader   = line_shader;
+        pc.line_VAO      = line_VAO;
+        pc.line_Indicies = num_of_verts;
+
+    } // LINE
 
     { // PLANE
         const Shader_t plane_Shader = Shader_Create("../../Examples/27 - Procedural/plane.vs",
@@ -180,41 +364,43 @@ void Procedural_Init()
                                                         {.index = 0, .name = "aPos"},
                                                     });
 
-        const GLuint num_of_divisons = 6;
+        const GLuint num_of_divisons = 10;
         const float  circle_radius   = 0.5f;
 
         // v3 --- v2
         // |      |
         // |      |
         // v0 --- v1
-        vec3 v0 = {-0.5f, -0.5f, 0.0f};
-        vec3 v1 = {0.5f, -0.5f, 0.0f};
-        vec3 v2 = {0.5f, 0.5f, 0.0f};
-        vec3 v3 = {-0.5f, 0.5f, 0.0f};
+        vec3 v0 = {-1.0f, 2.0f, 1.0f};
+        vec3 v1 = {1.0f, 0.0f, 1.0f};
+        vec3 v2 = {1.0f, 0.0f, -1.0f};
+        vec3 v3 = {-1.0f, 0.0f, -1.0f};
 
-        GLuint num_of_indicies = getIndicesCount(4, 4);
+        GLuint num_of_verts = (num_of_divisons + 1) * (num_of_divisons + 1);
+        GLuint num_of_indi;
 
-        // GLfloat *verticies = get_plane_vertices(4, 4);
-        GLfloat *verticies = Plane_Verticies(v0, v1, v2, v3, 4);
-        // GLuint  *indicies  = Circle_Indicies(num_of_indicies);
-        GLuint *indicies = get_plane_Indices(4, 4, &num_of_indicies);
+        GLfloat *verticies = Plane_Verticies(v3, v2, v1, v0, num_of_divisons);
+        GLuint  *indicies  = get_plane_Indices(num_of_divisons + 1, num_of_divisons + 1, &num_of_indi);
 
         VAO_t plane_VAO = VAO_Create();
 
         VBO_t plane_VBO = VBO_Create(GL_ARRAY_BUFFER);
-        VBO_Buffer(plane_VBO, sizeof(GLfloat) * num_of_indicies * 3, (const GLvoid *)verticies);
+        VBO_Buffer(plane_VBO, sizeof(GLfloat) * num_of_verts * 3, (const GLvoid *)verticies);
 
         EBO_t plane_EBO = EBO_Create();
-        EBO_Buffer(plane_EBO, sizeof(GLuint) * num_of_indicies, (const GLvoid *)indicies);
+        EBO_Buffer(plane_EBO, sizeof(GLuint) * num_of_indi, (const GLvoid *)indicies);
 
         VAO_Attr(plane_VAO, plane_VBO, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)0);
 
-        // free(verticies);
-        // free(indicies);
+        free(verticies);
+        free(indicies);
+
+        const Camera_t cam = Camera_Create(window.width, window.heigh, (vec3){0.0f, 0.0f, 2.0f}, 45.0f, 0.1f, 1000.0f);
+        pc.cam             = cam;
 
         pc.plane_Shader   = plane_Shader;
         pc.plane_VAO      = plane_VAO;
-        pc.plane_Indicies = num_of_indicies;
+        pc.plane_Indicies = num_of_indi;
     } // PLANE
 }
 
@@ -226,11 +412,24 @@ void Procedural_Update()
     //    glDrawArrays(GL_TRIANGLE_FAN, 0, pc.circle_Count);
     //} // CIRCLE
 
+    //{ // LINE
+    //    Shader_Bind(pc.line_Shader);
+    //    VAO_Bind(pc.line_VAO);
+    //    // glDrawArrays(GL_POINTS, 0, pc.line_Indicies);
+    //    glDrawElements(GL_POINTS, pc.line_Indicies, GL_UNSIGNED_INT, 0);
+    //} // LINE
+
     { // PLANE
+        Camera_Inputs(&pc.cam);
+
         Shader_Bind(pc.plane_Shader);
+
+        Camera_View_Projection_To_Shader(pc.cam, pc.plane_Shader, "camMatrix");
+
         VAO_Bind(pc.plane_VAO);
-        // glDrawArrays(GL_TRIANGLE_STRIP, 0, pc.plane_Indicies);
-        glDrawElements(GL_POINTS, pc.plane_Indicies, GL_UNSIGNED_INT, 0);
+        // glDrawArrays(GL_LINE_STRIP, 0, pc.plane_Indicies);
+        glDrawElements(GL_LINE_STRIP, pc.plane_Indicies, GL_UNSIGNED_INT, 0);
+
     } // PLANE
 }
 
@@ -240,6 +439,8 @@ void Procedural_OnExit()
 
     Shader_Destroy(&pc.circle_Shader);
     VAO_Destroy(pc.circle_VAO);
+    Shader_Destroy(&pc.line_Shader);
+    VAO_Destroy(pc.line_VAO);
     Shader_Destroy(&pc.plane_Shader);
     VAO_Destroy(pc.plane_VAO);
 }
--- Examples/27 - Procedural/plane.vs
+++ Examples/27 - Procedural/plane.vs
@@ -2,7 +2,10 @@
 
 layout(location = 0) in vec3 aPos;
 
+// Imports the camera matrix from the main function
+uniform mat4 camMatrix;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = camMatrix * vec4(aPos, 1.0);
 }
