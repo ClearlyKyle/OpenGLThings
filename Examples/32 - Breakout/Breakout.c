6aa9bc3b2906e471ea50fa7c8f9e7315bbd1abdd
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-06-19 00:11:31 +0100

    > fixed glm_ortho matrix > fixed type issues > simplified shader

--- Examples/32 - Breakout/Breakout.c
+++ Examples/32 - Breakout/Breakout.c
@@ -6,6 +6,7 @@ struct Circle
 {
     Shader_t shader;
     VAO_t    vao;
+    GLsizei  count;
 
     float dx;
     float dy;
@@ -15,25 +16,27 @@ struct Circle
 
 void Breakout_Init()
 {
-    GLfloat circle_vertices[6 * NUM_SEGMENTS];
+    GLfloat circle_vertices[6 * NUM_SEGMENTS] = {0};
 
     for (int i = 0; i < NUM_SEGMENTS; i++)
     {
-        const float angle     = i * 2.0f * M_PI / (NUM_SEGMENTS - 1);
-        const float nextAngle = (i + 1) * 2.0f * M_PI / (NUM_SEGMENTS - 1);
+        const double angle     = i * 2.0 * M_PI / (double)(NUM_SEGMENTS - 1);
+        const double nextAngle = (i + 1) * 2.0 * M_PI / (double)(NUM_SEGMENTS - 1);
 
         // Vertex 1
-        circle_vertices[i * 6 + 0] = cos(angle) * 20; // X
-        circle_vertices[i * 6 + 1] = sin(angle) * 20; // Y
+        circle_vertices[i * 6 + 0] = (GLfloat)(cos(angle) * 20.0); // X
+        circle_vertices[i * 6 + 1] = (GLfloat)(sin(angle) * 20.0); // Y
 
         // Vertex 2
-        circle_vertices[i * 6 + 2] = cos(nextAngle) * 20; // X
-        circle_vertices[i * 6 + 3] = sin(nextAngle) * 20; // Y
+        circle_vertices[i * 6 + 2] = (GLfloat)(cos(nextAngle) * 20.0); // X
+        circle_vertices[i * 6 + 3] = (GLfloat)(sin(nextAngle) * 20.0); // Y
 
         // Vertex 3
         circle_vertices[i * 6 + 4] = 0.0f; // Origin
         circle_vertices[i * 6 + 5] = 0.0f;
     }
+
+    circle.count = 6 * NUM_SEGMENTS;
 
     const Shader_t shader = Shader_Create("../../Examples/32 - Breakout/default.vs",
                                           "../../Examples/32 - Breakout/default.fs",
@@ -43,15 +46,18 @@ void Breakout_Init()
                                           });
 
     VAO_t circle_VAO = VAO_Create();
-    VBO_t circle_VBO = VBO_Create(GL_ARRAY_BUFFER);
 
+    VBO_t circle_VBO = VBO_Create(GL_ARRAY_BUFFER);
     VBO_Buffer(circle_VBO, sizeof(circle_vertices), (const GLvoid *)circle_vertices);
 
-    VAO_Attr(circle_VAO, circle_VBO, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)0);
+    VAO_Attr(circle_VAO, circle_VBO, 0, 2, GL_FLOAT, 2 * sizeof(GLfloat), (const GLvoid *)0);
 
-    mat4 orthograph;
-    // mat4_orthagonal(WIDTH, HEIGHT, orthograph);
-    glm_ortho_default(window.aspect_ratio, orthograph);
+    // mat4 orthograph;
+    //// mat4_orthagonal(WIDTH, HEIGHT, orthograph);
+    // glm_ortho_default(window.aspect_ratio, orthograph);
+
+    mat4 orthograph = GLM_MAT4_IDENTITY_INIT;
+    glm_ortho(0.0f, window.width, window.heigh, 0.0f, -1.0f, 1.0f, orthograph);
 
     Shader_Bind(shader);
     Shader_Uniform_Mat4(shader, "orthograph", orthograph);
@@ -69,12 +75,19 @@ void Breakout_Init()
 
 void Breakout_Update()
 {
-    circle.pos[0] += circle.dx;
-    circle.pos[1] += circle.dy;
+    // if (Input_Key_Down(window.input, SDL_SCANCODE_D))
+    //{
+    //     glm_vec3_muladd(speed, camera->orientation, camera->position);
+    // }
 
-    if (circle.pos[0] > window.width)
+    const double tm = window.frame_time * 10.0;
+
+    circle.pos[0] += (circle.dx * (float)tm);
+    circle.pos[1] += (circle.dy * (float)tm);
+
+    if (circle.pos[0] > (float)window.width)
     {
-        circle.pos[0] = window.width;
+        circle.pos[0] = (float)window.width;
         circle.dx *= -1;
     }
     else if (circle.pos[0] < 0)
@@ -83,9 +96,9 @@ void Breakout_Update()
         circle.dx *= -1;
     }
 
-    if (circle.pos[1] > window.heigh)
+    if (circle.pos[1] > (float)window.heigh)
     {
-        circle.pos[1] = window.heigh;
+        circle.pos[1] = (float)window.heigh;
         circle.dy *= -1;
     }
     else if (circle.pos[1] < 0)
@@ -94,14 +107,15 @@ void Breakout_Update()
         circle.dy *= -1;
     }
 
-    mat4 mvp;
-    glm_translate_make(mvp, circle.pos);
+    mat4 model;
+    glm_translate_make(model, circle.pos);
 
     Shader_Bind(circle.shader);
-    Shader_Uniform_Mat4(circle.shader, "", mvp);
+    Shader_Uniform_Mat4(circle.shader, "model", model);
+    Shader_Uniform_Vec3(circle.shader, "pos", circle.pos);
 
     VAO_Bind(circle.vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3 * NUM_SEGMENTS);
+    glDrawArrays(GL_TRIANGLES, 0, circle.count);
 }
 
 void Breakout_OnExit()
--- Examples/32 - Breakout/default.vs
+++ Examples/32 - Breakout/default.vs
@@ -1,13 +1,15 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos;
+layout(location = 0) in vec2 aPos;
 
-uniform mat4   mvp;
-uniform mat4   orthograph;
-attribute vec2 coord2d;
+uniform mat4 model;
+uniform mat4 orthograph;
+uniform vec3 pos;
 
 void main()
 {
-    vec4 pixel_pos = mvp * vec4(coord2d, 0.0, 1.0);
-    gl_Position    = orthograph * pixel_pos;
+    // vec3 update_pos = vec3(aPos, 0.0) + pos;
+    // vec4 pixel_pos  = mvp * vec4(vec3(aPos, 0.0), 0.0);
+    //  gl_Position     = orthograph * pixel_pos;
+    gl_Position = orthograph * model * vec4(aPos.xy, 0.0, 1.0);
 }
