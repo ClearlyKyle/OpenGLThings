3a216eab8079f94eeb602e2dc5b8724c9d04141a
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-07-15 15:10:22 +0100

    Added sphere to shapes

--- Engine/Shapes.c
+++ Engine/Shapes.c
@@ -88,3 +88,89 @@ VAO_t Quad_Generate()
 
     return VAO;
 };
+
+VAO_t Sphere_Generate()
+{
+    const unsigned int X_SEGMENTS = 64;
+    const unsigned int Y_SEGMENTS = 64;
+
+    const size_t size = (X_SEGMENTS + 1) * (Y_SEGMENTS + 1) * 3;
+
+    GLfloat      *positions  = (GLfloat *)malloc(sizeof(GLfloat) * size);
+    GLfloat      *normals    = (GLfloat *)malloc(sizeof(GLfloat) * size);
+    GLfloat      *tex_coords = (GLfloat *)malloc(sizeof(GLfloat) * (Y_SEGMENTS + 1) * (X_SEGMENTS + 1) * 2);
+    unsigned int *indices    = (unsigned int *)malloc(sizeof(unsigned int) * Y_SEGMENTS * (X_SEGMENTS + 1) * 2);
+
+    unsigned int k         = 0;
+    unsigned int tex_index = 0;
+    for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+    {
+        for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            const float xSegment = (float)x / (float)X_SEGMENTS;
+            const float ySegment = (float)y / (float)Y_SEGMENTS;
+            const float xPos     = cosf(xSegment * 2.0f * (float)M_PI) * sinf(ySegment * (float)M_PI);
+            const float yPos     = cosf(ySegment * (float)M_PI);
+            const float zPos     = sinf(xSegment * 2.0f * (float)M_PI) * sinf(ySegment * (float)M_PI);
+
+            positions[k + 0] = xPos;
+            positions[k + 1] = yPos;
+            positions[k + 2] = zPos;
+
+            normals[k + 0] = xPos;
+            normals[k + 1] = yPos;
+            normals[k + 2] = zPos;
+            k += 3;
+
+            tex_coords[tex_index++] = xSegment;
+            tex_coords[tex_index++] = ySegment;
+            // uv.push_back(glm::vec2(xSegment, ySegment));
+        }
+    }
+
+    bool         oddRow = false;
+    unsigned int index  = 0;
+    for (unsigned int y = 0; y < Y_SEGMENTS; y++)
+    {
+        if (!oddRow) // even rows: y == 0, y == 2; and so on
+        {
+            for (unsigned int x = 0; x <= X_SEGMENTS; x++)
+            {
+                indices[index++] = y * (X_SEGMENTS + 1) + x;
+                indices[index++] = (y + 1) * (X_SEGMENTS + 1) + x;
+            }
+        }
+        else
+        {
+            for (int x = X_SEGMENTS; x >= 0; x--)
+            {
+                indices[index++] = (y + 1) * (X_SEGMENTS + 1) + x;
+                indices[index++] = y * (X_SEGMENTS + 1) + x;
+            }
+        }
+        oddRow = !oddRow;
+    }
+
+    VAO_t sphere = VAO_Create();
+
+    EBO_t indi = EBO_Create();
+    EBO_Buffer(indi, sizeof(GLfloat) * X_SEGMENTS * (Y_SEGMENTS + 1) * 2, (const GLvoid *)indices);
+
+    VBO_t verts = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(verts, sizeof(GLfloat) * size, (const GLvoid *)positions);
+    VBO_t norm = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(norm, sizeof(GLfloat) * size, (const GLvoid *)normals);
+    VBO_t tex = VBO_Create(GL_ARRAY_BUFFER);
+    VBO_Buffer(tex, sizeof(GLfloat) * Y_SEGMENTS * (X_SEGMENTS + 1) * 2, (const GLvoid *)tex_coords);
+
+    VAO_Attr(sphere, verts, 0, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)(0));
+    VAO_Attr(sphere, norm, 1, 3, GL_FLOAT, 3 * sizeof(GLfloat), (const GLvoid *)(0));
+    VAO_Attr(sphere, tex, 2, 2, GL_FLOAT, 2 * sizeof(GLfloat), (const GLvoid *)(0));
+
+    free(positions);
+    free(normals);
+    free(indices);
+    free(tex_coords);
+
+    return sphere;
+}
--- Engine/Shapes.h
+++ Engine/Shapes.h
@@ -9,10 +9,11 @@
 
 VAO_t Cube_Generate();
 VAO_t Quad_Generate();
+VAO_t Sphere_Generate();
 
 inline void Cube_Draw(const VAO_t cube_vao)
 {
-    // Draw cube
+    // Draw Cube
     VAO_Bind(cube_vao);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 }
@@ -22,7 +23,14 @@ inline void Quad_Draw(const VAO_t quad_vao)
     // Draw Quad
     VAO_Bind(quad_vao);
     // glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLE_STRIP, 6, GL_UNSIGNED_INT, 0);
+}
+
+inline void Sphere_Draw(const VAO_t sphere_vao)
+{
+    // Draw Sphere
+    VAO_Bind(sphere_vao);
+    glDrawElements(GL_TRIANGLE_STRIP, 8320, GL_UNSIGNED_INT, 0);
 }
 
 #endif // __SHAPES_H__
