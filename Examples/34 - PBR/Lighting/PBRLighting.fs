1c461015a5feade651bd438d80ac6364f933b612
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-07-26 22:02:17 +0100

    formatting

--- Examples/34 - PBR/Lighting/PBRLighting.fs
+++ Examples/34 - PBR/Lighting/PBRLighting.fs
@@ -2,9 +2,9 @@
 
 out vec4 FragColor;
 
-in vec2  TexCoords;
-in vec3  WorldPos;
-in vec3  Normal;
+in vec2 TexCoords;
+in vec3 WorldPos;
+in vec3 Normal;
 
 // material parameters
 uniform vec3  albedo;
